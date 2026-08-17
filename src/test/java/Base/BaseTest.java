4af2e781ec5adb59f89fb354dd1f8198cdb10b57
indirakumarwebdevelper <155740936+indirakumarwebdevelper@users.noreply.github.com>
2026-08-17 18:07:29 +0530

    Update BaseTest.java

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -2,7 +2,7 @@ package Base;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Map;   //test
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
